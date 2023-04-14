e27e08701cb1e9cb9dedddb861e41d44f9e6dd28
Mohitpg <mohitp.gupta1102@gmail.com>
2023-04-14 14:46:40 +0530

    added fade-in effect

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var i = 0;
 var txt = '-A (future) software engineer'; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+var speed = 60; /* The speed/duration of the effect in milliseconds */
+const faders = document.querySelectorAll(".fadeIn");
+
+const scrollops={
+  threshold:1
+}
+
+const scroll= new IntersectionObserver(function(entries,scroll){
+  entries.forEach(entry =>{
+    if (entry.isIntersecting){
+      entry.target.classList.add("appear");
+      scroll.unobserve(entry.target);
+    }
+  })
+},scrollops);
+
+faders.forEach(fader =>{
+  console.log(fader);
+  scroll.observe(fader);
+})
 
 function typeWriter() {
   if (i < txt.length) {
